8513e342ecebab0a2ddad136f20b6f963bc3fcbc
Vivallo04 <andresvivallo4@gmail.com>
2022-02-01 23:11:26 -0600

    [Update] Add Heroku deploy configuration

--- config/config.js
+++ config/config.js
@@ -7,10 +7,10 @@
  */
 const config = {
     env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 5000,
     jwtSecret: process.env.JWT_SECRET || "Study_Key",
     mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST ||
-        'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + '/studyroom-development'
+        'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + '/studyroom-development',
 };
 
 export default config;
