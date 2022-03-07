a90ac9e0338871850a55e0f890afc4b2022144db
Vivallo04 <andresvivallo4@gmail.com>
2022-03-07 16:54:25 -0600

    Added Menu back to main screens

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import {Route, Switch} from 'react-router-dom'
-import Home from './core/Home'
-import Users from './user/Users'
-import Signup from './user/Signup'
-import Signin from './auth/Signin'
-import EditProfile from './user/EditProfile'
-import Profile from './user/Profile'
-import PrivateRoute from './auth/PrivateRoute'
-import { AppBar } from '@material-ui/core'
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Home from './core/Home';
+import Users from './user/Users';
+import Signup from './user/Signup';
+import Signin from './auth/Signin';
+import EditProfile from './user/EditProfile';
+import Profile from './user/Profile';
+import PrivateRoute from './auth/PrivateRoute';
+import Menu from './core/Menu';
 
 const MainRouter = () => {
     return (
     <div>
-        <AppBar/>
+        <Menu/>
         <Switch>
             <Route exact path="/" component={Home}/>
             <Route path="/users" component={Users}/>
